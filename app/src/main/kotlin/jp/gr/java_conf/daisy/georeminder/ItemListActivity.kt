952ba6c015302ccef243484cca68a1a2a4e5cd89
daisy1754 <d-part@hotmail.co.jp>
2015-12-03 23:24:50 -0800

    Convert project to kotlin

---
+++ app/src/main/kotlin/jp/gr/java_conf/daisy/georeminder/ItemListActivity.kt
@@ -0,0 +1,13 @@
+package jp.gr.java_conf.daisy.georeminder
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import jp.gr.java_conf.daisy.georeminder.R
+
+class ItemListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_list)
+    }
+}
