1adae45b10c688fd92b7dc3694c4e6a5c901aa31
daisy1754 <d-part@hotmail.co.jp>
2016-02-28 13:21:42 -0800

    Add FAB in main activity to add new reminder

--- app/src/main/kotlin/jp/gr/java_conf/daisy/georeminder/ItemListActivity.kt
+++ app/src/main/kotlin/jp/gr/java_conf/daisy/georeminder/ItemListActivity.kt
@@ -1,13 +1,16 @@
 package jp.gr.java_conf.daisy.georeminder
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import jp.gr.java_conf.daisy.georeminder.R
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_item_list.*
+import org.jetbrains.anko.startActivity
 
 class ItemListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_list)
+
+        fab.setOnClickListener { startActivity<NewReminderActivity>() }
     }
 }
---
+++ app/src/main/kotlin/jp/gr/java_conf/daisy/georeminder/NewReminderActivity.kt
@@ -0,0 +1,12 @@
+package jp.gr.java_conf.daisy.georeminder
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class NewReminderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_reminder)
+    }
+}
